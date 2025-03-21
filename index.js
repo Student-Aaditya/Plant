75cda483d0cb047b23c02058e21c01229b4f99ed
Aaditya <aadityakuamr518@gmail.com>
2025-03-22 02:36:46 +0530

    add

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app=express();
 const port=8020;
 const path=require("path");
 const ejsMate=require("ejs-mate");
+const methodOverride=require("method-override");
+const mongoose = require("mongoose");
+const User=require("./Model/user.js");
+const passport=require("passport");
+const passportLocal=require("passport-local");
+const session=require("express-session");
 
 app.set("views",path.join(__dirname,"view"));
 app.set("view engine","ejs");
@@ -11,6 +17,33 @@ app.engine("ejs", ejsMate);
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+const sessionOption = ({
+    secret: "musecretcode",
+    resave: false,
+    saveUninitialized: true,
+})
+
+app.use(session(sessionOption));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new passportLocal(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/Plant");
+
+}
+main().
+    then(() => {
+        console.log("sucessful connection");
+    }).catch((err) => {
+        console.log(err);
+    })
 
 
 app.get("/plant",(req,res)=>{
@@ -25,8 +58,26 @@ app.get("/login",(req,res)=>{
     res.render("login.ejs");
 })
 
+app.post("/login", passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }), async (req, res) => {
+    console.log("login");
+    res.redirect("/");
+})
+
 app.get("/sign",(req,res)=>{
     res.render("sign-up.ejs");
+})
+
+app.post("/sign",async (req,res)=>{
+    try{
+        let {username,email,password}=req.body;
+        const newUser=new User({email,username});
+        const register=await User.register(newUser,password);
+        res.redirect("/");
+        console.log(register);
+    }catch(err){
+        console.log(err);
+        res.redirect("/sign");
+    }
 })
 
 app.listen(port,(req,res)=>{
